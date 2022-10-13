7a14b962bb758be4e7d56c099ea32774535d91e2
Ellal <ellaljus@hotmail.comm>
2022-10-13 12:28:49 +0200

    js

--- script.js
+++ script.js
@@ -1,26 +1,80 @@
-const name = document.getElementById('name')
-const mail = document.getElementById('mail')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
+const onSubmit = event => {
+    event.preventDefault()
+    validate(event)
+}
 
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (name.value === '' || name.value == null) {
-        messages.push('Name is required')
+
+const validate = (event) => {
+    switch(event.type) {
+        case 'keyup':
+            validateElement(event.target)
+            break;
+
+        case 'submit':
+            for(let element of event.target)
+                validateElement(element)
+            break;
     }
+}
 
-    if (name.value.length <= 1) {
-        messages.push('Minimum two letters')
+const validateElement = (element) => {
+    if (element.required) {
+        let label = document.getElementById(`${element.id}-label`).innerText
+        let error = ""
+
+        switch(element.type) {
+            case 'text':
+                if (!isNullOrEmpty(element.value)) {
+                    if (!isMinimumLength(element.value, element.dataset.requiredMin)) {
+                        error = `Must contain at least 2 letters.`
+                    }
+                } else {
+                    error = `You must enter a name`
+                }
+                break;
+
+            case 'email':        
+                if (!isNullOrEmpty(element.value)) {
+                    if(!isEmailValid(element.value)) {
+                        error = `Must be valid (eg. example@domain.com)`
+                    }
+                } else {
+                    error = `You must enter an email`
+                }
+                
+                break;
+        }
+
+
+        document.getElementById(`${element.id}-error`).innerText = error
     }
-
-    if (name.value.length >= 20) {
-        messages.push('Damn thats to long')
-    }
+}
+   
 
 
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
-    }
-    
-})
+
+
+
+
+const isNullOrEmpty = value => {
+    if (value.length === 0)
+        return true
+
+    return false
+}
+
+const isMinimumLength = (value, minLength = 2) => {
+    if (value.length >= minLength)
+        return true
+
+    return false
+}
+
+const isEmailValid  = (email) => {
+    const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    if (regEx.test(email))
+        return true
+
+    return false
+}
